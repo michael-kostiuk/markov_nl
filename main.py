e62c1b5eb69ff7552e952f0758c7f6d2101daebe
Kostuik Michael <michael@bmetric.com>
2019-08-07 01:32:08 +0300

    add spellcheck

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import json
 import time
+import re
 
-ALL = 'ALL'
+from functools import reduce
+
 
 TRAIN_DIR = 'out'
+MINIMAL_WORD_FREQ_THRESHOLD = 0.0001
+MARKOV_PROBABILY_THRESHOLD = 0.0003
+
+
+def memo(f):
+    "Memoize function f, whose args must all be hashable."
+    cache = {}
+    def fmemo(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    fmemo.cache = cache
+    return fmemo
 
 
 class WordReader(object):
@@ -22,6 +37,9 @@ class WordReader(object):
     def __iter__(self):
         self.chached_chunk = self.__file_object.read(self.chunk_size)
         return self
+
+    def reset(self):
+        self.__file_object.seek(0)
 
     def __next__(self):
         word = ''
@@ -44,6 +62,7 @@ class WordReader(object):
                     elif word:
                         word_completed = True
                         self.chached_chunk = chunk[i:] + self.__file_object.read(i + 1)
+                        break
                 except IndexError:
                     # file ended before chunk
                     raise StopIteration
@@ -56,14 +75,15 @@ class MarkovModel(object):
     dump_filename = 'model.json'
 
     @classmethod
-    def from_dump(cls, order):
-        m = cls(order)
+    def from_dump(cls):
         with open(cls.dump_filename, 'r') as f:
-            m.graph = json.loads(f.read())
+            data = json.loads(f.read())
+            m = cls(data['orders'])
+            m.graph = data['graph']
 
         return m
 
-    def __init__(self, orders, filename=None):
+    def __init__(self, orders=[], filename=None):
         self.graph = {}
         self.orders = orders
 
@@ -81,17 +101,29 @@ class MarkovModel(object):
                         symb = word[i+order]
                         self.graph.setdefault(kgram, {}).setdefault(symb, 0)
                         self.graph[kgram][symb] += 1
-                        self.graph[kgram].setdefault(ALL, 0)
-                        self.graph[kgram][ALL] += 1
+                        self.graph[kgram].setdefault('ALL', 0)
+                        self.graph[kgram]['ALL'] += 1
 
     def freq_part(self, part):
-        return sum(self.graph[part])
-
-    def freq(self, part, symb):
         try:
-            return self.graph[part][symb] / self.graph[part][ALL]
+            kk = self.graph[part].keys()
         except KeyError:
             return 0
+
+        N = kk.pop('ALL')
+        return sum(kk.values())
+
+    def next_for_part(self, part):
+        try:
+            kk = list(self.graph[part].keys())
+        except KeyError:
+            return []
+        kk.remove('ALL')
+        return kk
+
+    def freq(self, part, symb):
+        """Return smoothed frequency how many times symbol appears after kgram"""
+        return (self.graph.get(part, {}).get(symb, 0) + 1) / self.graph.get(part, {'ALL': len(self.graph)})['ALL']
 
     def pprint(self):
         out = ['%s: %s' % (k, v) for k, v in self.graph.items()]
@@ -103,40 +135,167 @@ class MarkovModel(object):
                 f2.write(f1.read())
 
         with open(self.dump_filename, 'w') as f:
-            f.write(json.dumps(self.graph))
+            dump_data = {'graph': self.graph, 'orders': self.orders}
+            f.write(json.dumps(dump_data))
 
 
-def spellcheck(m):
-    import getch
+class Spellcheck(object):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-    iinput = ''
-    checking = ''
-    input('GO:')
-    while True:
-        ch = getch.getch()
-        if ch == ' ':
-            checking = ''
-        freq = 1
-        if len(checking) > 4:
-            freq = m.freq(iinput[-4:], ch)
-        iinput += ch
-        checking += ch
-        sys.stdout.write('%s(%10.2f)\r' % (iinput, freq))
+    def __init__(self, words_bag, model):
+        self.words_bag = words_bag
+        self.model = model
+        self.N = sum(words_bag.values())
+
+    def edits1(self, word):
+        "Return all strings that are one edit away from this word."
+        pairs      = self.splits(word)
+        deletes    = [a+b[1:]           for (a, b) in pairs if b]
+        transposes = [a+b[1]+b[0]+b[2:] for (a, b) in pairs if len(b) > 1]
+        replaces   = [a+c+b[1:]         for (a, b) in pairs for c in Spellcheck.alphabet if b]
+        inserts    = [a+c+b             for (a, b) in pairs for c in Spellcheck.alphabet]
+        return set(deletes + transposes + replaces + inserts)
+
+    def editsN(self, word, num):
+        edits = {word}
+        for _ in range(num):
+            temp = set()
+            for edit in edits:
+                temp.update(self.edits1(edit))
+            edits.update(temp)
+        return edits
+
+    def splits(self, text, start=0, L=20):
+        "Return a list of all (first, rest) pairs; start <= len(first) <= L."
+        return [(text[:i], text[i:]) for i in range(start, min(len(text), L)+1)]
+
+    def freq(self, word):
+        try:
+            return self.words_bag[word] / self.N
+        except KeyError:
+            return 0
+
+    def p_words(self, words):
+        return self._product([self.freq(w) for w in words])
+
+    def _product(self, l):
+        result = 1
+        for el in l:
+            result *= el
+        return result
+
+    @memo
+    def segment(self, text):
+        "Return a list of words that is the most probable segmentation of text."
+        if not text:
+            return []
+        else:
+            candidates = [[first] + self.segment(rest) for (first, rest) in self.splits(text, 1)]
+            return max(candidates, key=self.p_words)
+
+    def looks_like_word(self, word):
+        l_word = len(word)
+
+        if all([order < l_word for order in self.model.orders]):
+            # cant check word less then order of model
+            return False
+
+        probability = 1
+        for order in self.model.orders:
+            for i in range(l_word-order):
+                kgram = word[i:i+order]
+                probability *= self.model.freq(kgram, word[i+order])
+        return probability > MARKOV_PROBABILY_THRESHOLD
+
+    def correct(self, word, edits_depth=2):
+        if self.looks_like_word(word):
+            return word
+
+        options = []
+        if self.freq(word) > MINIMAL_WORD_FREQ_THRESHOLD:
+            options.append((self.freq(word), word))
+        else:
+            word_fixed = max(self.editsN(word, edits_depth), key=lambda x: self.freq(x))
+            options.append((self.freq(word_fixed), word_fixed))
+
+        # check if multiple words collapsed
+        out = []
+        for word_candidate in self.segment(word):
+            out.append(word_candidate)
+
+
+        options.append((sum([self.freq(x) for x in out]), ' '.join(out)))
+        return max(options)[1]
+
+    def correct_match(self, match):
+        "Spell-correct word in match, and preserve proper upper/lower/title case."
+        word = match.group()
+        return self.case_of(word)(self.correct(word.lower()))
+
+    def case_of(self, text):
+        "Return the case-function appropriate for text: upper, lower, title, or just str."
+        return (str.upper if text.isupper() else
+                str.lower if text.islower() else
+                str.title if text.istitle() else
+                str)
+
+    def check_text(self, text):
+        "Correct all the words within a text, returning the corrected text."
+        return re.sub('[a-zA-Z]+', self.correct_match, text)
 
 
 if __name__ == "__main__":
     import sys
     from os import listdir, path
+    from collections import Counter
 
-    docs = listdir(TRAIN_DIR)
-    file_count = len(docs)
-    processed = 0
+    # docs = listdir(TRAIN_DIR)
+    # file_count = len(docs)
+    # processed = 0
 
-    m = MarkovModel([3, ])
 
-    for doc in docs:
-        m.feed(path.join(TRAIN_DIR, doc))
-        processed += 1
-        sys.stdout.write('\r[%2.2f%%]' % (processed / file_count * 100))
+    # heavy calculate
+    # model = MarkovModel([3, 5, 7])
+    # bag = Counter()
+    # for doc in docs:
+    #     model.feed(path.join(TRAIN_DIR, doc))
+        # with WordReader(path.join(TRAIN_DIR, doc)) as w:
+        #     for word in w:
+        #         bag[word] += 1
+    #     processed += 1
+    #     sys.stdout.write('\r[%2.2f%%]' % (processed / file_count * 100))
+    # model.save()
 
-    # m.save()
+    # or fast restore from dump
+    m = MarkovModel.from_dump()
+
+    with open('words_bag_dump.json', 'r') as f:
+        bag = Counter(json.loads(f.read()))
+
+    spellcheck = Spellcheck(bag, m)
+    text = """
+            The Zen of Python, by Tim Peters
+
+            Beauteaful is bettor thanugly.
+            Explicit isbetter than implicit.
+            Simple is betterthan complex.
+            Complex is better thancomplicated.
+            Flat is better than nested.
+            Sparse is better than dense.
+            Readabilitie counts.
+            Speciall cases aren't speciial enouugh to breakk othoe rulles.
+            Although practicality beats purity.
+            Errors should never pass silently.
+            Unless explicitly silenced.
+            In the face of ambiguity, refuse the temptation to guess.
+            There should be one-- and preferably only one --obvious way to do it.
+            Although that way may not be obvious at first unless you're Dutch.
+            Now is better than never.
+            Although never is often better than *right* now.
+            If the implementation is hard to explain, it's a bad idea.
+            If the implementation is easy to explain, it may be a good idea.
+            Namespaces are one honking great idea -- let's do more of those!
+            """
+    # text = input()
+    corrected_text = spellcheck.check_text(text)
+    print(corrected_text)
